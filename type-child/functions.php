65af0ce8ffa292942206be8194149ac9d108011d
yhassan <yasmhas@gmail.com>
2018-01-17 20:36:40 -0700

    Added 2 Custom Taxonomies

--- type-child/functions.php
+++ type-child/functions.php
@@ -1,19 +1,24 @@
 <?php
 function my_theme_enqueue_styles() {
 
-    $parent_style = 'parent-style'; // This is type-style | Type is Parent Theme
+    $parent_style = 'type-style'; // This is type-style | Type is Parent Theme
 
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
+        array( $parent_style )
     );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 
-// Registering Custom Post Type [Post Type = Posters --> Capstone]
+/*
+  =========================================================================
+  Custom Post Type
+  =========================================================================
+*/
+
+// Custom Post Type [Post Type = Posters --> Capstone]
 function custom_post_type() {
 
 	$labels = array(
@@ -49,9 +54,10 @@ function custom_post_type() {
 		'label'                 => __( 'Posters', 'type-child' ),
 		'description'           => __( 'Post Type Description', 'type-child' ),
 		'labels'                => $labels,
-		'supports'              => array( 'title', 'editor' ),
-		'taxonomies'            => array( 'category', 'post_tag' ),
-		'hierarchical'          => false,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( 'post_tag', 'topics', 'publication' ),
+                                    // added 2 custom taxonomies here & deleted categories
+		'hierarchical'          => true,
 		'public'                => true,
 		'show_ui'               => true,
 		'show_in_menu'          => true,
@@ -62,10 +68,115 @@ function custom_post_type() {
 		'has_archive'           => true,
 		'exclude_from_search'   => false,
 		'publicly_queryable'    => true,
-		'capability_type'       => 'page',
+		'capability_type'       => 'post',
 	);
 	register_post_type( 'posters', $args );
 
 }
 add_action( 'init', 'custom_post_type', 0 );
+
+
+/*
+  =========================================================================
+  Taxonomy #1 --> Poster Topics
+  =========================================================================
+*/
+
+// Custom Taxonomy for Topics
+
+function project_topics_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Topics', 'Taxonomy General Name', 'type_child' ),
+		'singular_name'              => _x( 'Topic', 'Taxonomy Singular Name', 'type_child' ),
+		'menu_name'                  => __( 'Topics', 'type_child' ),
+		'all_items'                  => __( 'All Items', 'type_child' ),
+		'parent_item'                => __( 'Parent Item', 'type_child' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'type_child' ),
+		'new_item_name'              => __( 'New Item Name', 'type_child' ),
+		'add_new_item'               => __( 'Add New Item', 'type_child' ),
+		'edit_item'                  => __( 'Edit Item', 'type_child' ),
+		'update_item'                => __( 'Update Item', 'type_child' ),
+		'view_item'                  => __( 'View Item', 'type_child' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'type_child' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'type_child' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'type_child' ),
+		'popular_items'              => __( 'Popular Items', 'type_child' ),
+		'search_items'               => __( 'Search Items', 'type_child' ),
+		'not_found'                  => __( 'Not Found', 'type_child' ),
+		'no_terms'                   => __( 'No items', 'type_child' ),
+		'items_list'                 => __( 'Items list', 'type_child' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'type_child' ),
+	);
+	$rewrite = array(
+		'slug'                       => 'topics'
+	);
+
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+		'rewrite'                    => $rewrite,
+	);
+	register_taxonomy( 'topics', array( 'posters' ), $args );
+
+}
+add_action( 'init', 'project_topics_taxonomy', 0 );
+
+
+/*
+=========================================================================
+Taxonomy #2 -->  Publication
+=========================================================================
+*/
+
+// Custom Taxonomy for Publication (Term-Year)
+
+function publication_period_taxonomy() {
+
+	$labels = array(
+		'name'                       => _x( 'Publication Period', 'Taxonomy General Name', 'type_child' ),
+		'singular_name'              => _x( 'Publication', 'Taxonomy Singular Name', 'type_child' ),
+		'menu_name'                  => __( 'Publication', 'type_child' ),
+		'all_items'                  => __( 'All Items', 'type_child' ),
+		'parent_item'                => __( 'Parent Item', 'type_child' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'type_child' ),
+		'new_item_name'              => __( 'New Item Name', 'type_child' ),
+		'add_new_item'               => __( 'Add New Item', 'type_child' ),
+		'edit_item'                  => __( 'Edit Item', 'type_child' ),
+		'update_item'                => __( 'Update Item', 'type_child' ),
+		'view_item'                  => __( 'View Item', 'type_child' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'type_child' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'type_child' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'type_child' ),
+		'popular_items'              => __( 'Popular Items', 'type_child' ),
+		'search_items'               => __( 'Search Items', 'type_child' ),
+		'not_found'                  => __( 'Not Found', 'type_child' ),
+		'no_terms'                   => __( 'No items', 'type_child' ),
+		'items_list'                 => __( 'Items list', 'type_child' ),
+		'items_list_navigation'      => __( 'Items list navigation', 'type_child' ),
+	);
+	$rewrite = array(
+		'slug'                       => 'publication'
+	);
+
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+		'rewrite'                    => $rewrite,
+	);
+	register_taxonomy( 'publication', array( 'posters' ), $args );
+
+}
+add_action( 'init', 'publication_period_taxonomy', 0 );
+ 
 ?>
